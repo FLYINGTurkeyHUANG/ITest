76b88e562f5e2d0801ecdf18534bcdfd95781609
HDS <2534221987@qq.com>
2021-01-14 19:29:10 +0800

    2021-01-14:DEV:模板化思路

--- ITest/src/main/java/com/bj58/hds/template/TemplateField.java
+++ ITest/src/main/java/com/bj58/hds/template/TemplateField.java
@@ -21,5 +21,35 @@ public class TemplateField {
     /** 简历实体中字段的映射关系 */
     private String[] resumeField;
 
+    public String getFieldName() {
+        return fieldName;
+    }
 
+    public int getNecessary() {
+        return necessary;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public String[] getResumeField() {
+        return resumeField;
+    }
+
+    public void setFieldName(String fieldName) {
+        this.fieldName = fieldName;
+    }
+
+    public void setNecessary(int necessary) {
+        this.necessary = necessary;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public void setResumeField(String[] resumeField) {
+        this.resumeField = resumeField;
+    }
 }
