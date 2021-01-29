99a6691b9ef076328a8bdd77d516a787404e380f
HDS <2534221987@qq.com>
2021-01-29 11:43:26 +0800

    2021-01-29:DEV:JMX

--- ITest/src/main/java/com/bj58/hds/javabasic/designpattern/Main.java
+++ ITest/src/main/java/com/bj58/hds/javabasic/designpattern/Main.java
@@ -4,14 +4,14 @@ import java.util.concurrent.CountDownLatch;
 
 public class Main {
     public static void main(String[] args) {
-        CountDownLatch countDownLatch = new CountDownLatch(10000);
-        for(int i=0;i<10000;i++){
+        CountDownLatch countDownLatch = new CountDownLatch(2);
+        for(int i=0;i<2;i++){
             new Thread(){
                 @Override
                 public void run() {
                     try {
                         countDownLatch.await();
-                        System.out.println(Singleton.getInstance());
+                        System.out.println(Thread.currentThread().getName()+Singleton.getInstance());
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
